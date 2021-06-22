85ccee42923c7e455b1bc1ca25aefee6f5a39bf6
Adrien Pétremann <hello@grena.fr>
2021-06-22 16:15:13 +0200

    Add example for serializer

--- src/Api/ProductApi.php
+++ src/Api/ProductApi.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Heavymind\Oxatis\ApiClient\Api;
 
+use JMS\Serializer\SerializerBuilder;
+
 /**
  * API implementation to manage "ProductServices".
  *
@@ -14,5 +16,8 @@ class ProductApi implements ProductApiInterface
     public function ProductCount()
     {
         // TODO: Implement ProductCount() method.
+//        $serializer = SerializerBuilder::create()
+//            ->addMetadataDir('src/Metadata')
+//            ->build();
     }
 }
